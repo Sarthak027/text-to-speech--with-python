48aebd782fcf68461be8a8de6679b64145088e17
Sarthak <94513009+Sarthak027@users.noreply.github.com>
2022-12-23 18:49:07 +0530

    Create text-to-speech.py

---
+++ text-to-speech.py
@@ -0,0 +1,17 @@
+#Import the libraries
+from newspaper import Article
+import nltk
+from gtts import gTTS
+import os
+#Get the article
+article = Article('https://blog.bit.ai/top-technology-blogs/')
+article.download()
+article.parse()
+nltk.download('punkt')
+article.nlp()
+#Get the articles text
+mytext = article.text
+language = 'en' #English
+myobj = gTTS(text=mytext, lang=language, slow=False)
+myobj.save("read_article.mp3")
+os.system("start read_article.mp3")
